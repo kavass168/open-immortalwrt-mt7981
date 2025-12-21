d40eb278c59585c78543afe24e27298c89ef5759
kavass168 <631638801@qq.com>
2025-12-21 21:42:10 +0800

    Update copyfile-hy3000.sh

--- scripts/copyfile-hy3000.sh
+++ scripts/copyfile-hy3000.sh
@@ -6,5 +6,5 @@ cp $GITHUB_WORKSPACE/hy3000/mt7981b-philips-hy3000.dts ./openwrt/target/linux/me
 cp $GITHUB_WORKSPACE/hy3000/02_network ./openwrt/target/linux/mediatek/filogic/base-files/etc/board.d/
 cp $GITHUB_WORKSPACE/hy3000/platform.sh ./openwrt/target/linux/mediatek/filogic/base-files/lib/upgrade/        
 cp $GITHUB_WORKSPACE/hy3000/11-mt76-caldata ./openwrt/target/linux/mediatek/filogic/base-files/etc/hotplug.d/firmware/ 
-cp $GITHUB_WORKSPACE/hy3000/11_fix_wifi_mac ./openwrt/target/linux/mediatek/filogic/base-files/etc/hotplug.d/ieee80211/11_fix_wifi_mac/
+cp $GITHUB_WORKSPACE/hy3000/11_fix_wifi_mac ./openwrt/target/linux/mediatek/filogic/base-files/etc/hotplug.d/ieee80211/
 cp $GITHUB_WORKSPACE/hy3000/hy3000.conf ./openwrt/.config
