a76c0ae1cbdf27c5de8907ee1ce5ecec8e59c1d2
kavass168 <631638801@qq.com>
2025-12-23 00:24:52 +0800

    Update copyfile-hy3000.sh

--- scripts/copyfile-hy3000.sh
+++ scripts/copyfile-hy3000.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 # 3. 覆盖适配文件到官方源码
-cp $GITHUB_WORKSPACE/hy3000/filogic.mk ./openwrt/target/linux/mediatek/image/
-cp $GITHUB_WORKSPACE/hy3000/mt7981b-philips-hy3000.dts ./openwrt/target/linux/mediatek/dts/
-cp $GITHUB_WORKSPACE/hy3000/02_network ./openwrt/target/linux/mediatek/filogic/base-files/etc/board.d/
-cp $GITHUB_WORKSPACE/hy3000/platform.sh ./openwrt/target/linux/mediatek/filogic/base-files/lib/upgrade/        
-cp $GITHUB_WORKSPACE/hy3000/11-mt76-caldata ./openwrt/target/linux/mediatek/filogic/base-files/etc/hotplug.d/firmware/ 
-cp $GITHUB_WORKSPACE/hy3000/11_fix_wifi_mac ./openwrt/target/linux/mediatek/filogic/base-files/etc/hotplug.d/ieee80211/
-cp $GITHUB_WORKSPACE/hy3000/mediatek_filogic ./openwrt/package/boot/uboot-tools/uboot-envtools/files/
-cp $GITHUB_WORKSPACE/hy3000/hy3000.conf ./openwrt/.config
+cp $GITHUB_WORKSPACE/hy3000/filogic.mk $GITHUB_WORKSPACE/openwrt/target/linux/mediatek/image/
+cp $GITHUB_WORKSPACE/hy3000/mt7981b-philips-hy3000.dts $GITHUB_WORKSPACE/openwrt/target/linux/mediatek/dts/
+cp $GITHUB_WORKSPACE/hy3000/02_network $GITHUB_WORKSPACE/openwrt/target/linux/mediatek/filogic/base-files/etc/board.d/
+cp $GITHUB_WORKSPACE/hy3000/platform.sh $GITHUB_WORKSPACE/openwrt/target/linux/mediatek/filogic/base-files/lib/upgrade/        
+cp $GITHUB_WORKSPACE/hy3000/11-mt76-caldata $GITHUB_WORKSPACE/openwrt/target/linux/mediatek/filogic/base-files/etc/hotplug.d/firmware/ 
+cp $GITHUB_WORKSPACE/hy3000/11_fix_wifi_mac $GITHUB_WORKSPACE/openwrt/target/linux/mediatek/filogic/base-files/etc/hotplug.d/ieee80211/
+cp $GITHUB_WORKSPACE/hy3000/mediatek_filogic $GITHUB_WORKSPACE/openwrt/package/boot/uboot-tools/uboot-envtools/files/
+cp $GITHUB_WORKSPACE/hy3000/hy3000.conf $GITHUB_WORKSPACE/openwrt/.config
