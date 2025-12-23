ca897131f95b53e9a5269c4e992bc7d3ac1807ab
kavass168 <631638801@qq.com>
2025-12-23 21:09:32 +0800

    Update diy-seting.sh

--- scripts/diy-seting.sh
+++ scripts/diy-seting.sh
@@ -1,17 +1,5 @@
 #!/bin/bash
 
-PKG_PATH="$GITHUB_WORKSPACE/openwrt/package/"
-
-#修改argon主题字体和颜色
-if [ -d *"luci-theme-argon"* ]; then
-	echo " "
-
-	cd ./luci-theme-argon/
-
-	sed -i "s/primary '.*'/primary '#31a1a1'/; s/'0.2'/'0.5'/; s/'none'/'bing'/; s/'600'/'normal'/" ./luci-app-argon-config/root/etc/config/argon
-
-	cd $PKG_PATH && echo "theme-argon has been fixed!"
-fi
 
 
 
