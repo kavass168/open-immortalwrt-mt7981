30799d72b0b2c0413fee09d08f1cf1a86dfcc40b
kavass168 <631638801@qq.com>
2025-12-23 19:07:09 +0800

    Update diy-part2.sh

--- scripts/diy-part2.sh
+++ scripts/diy-part2.sh
@@ -1,14 +1,6 @@
 #!/bin/bash
 
 
-# UPDATE_PACKAGE "包名" "项目地址" "项目分支" "pkg/name，可选，pkg为从大杂烩中单独提取包名插件；name为重命名为包名"
-UPDATE_PACKAGE "argon" "sbwml/luci-theme-argon" "openwrt-24.10"
-#UPDATE_PACKAGE "aurora" "eamonxg/luci-theme-aurora" "master"
-#UPDATE_PACKAGE "aurora-config" "eamonxg/luci-app-aurora-config" "master"
-#UPDATE_PACKAGE "nikki" "nikkinikki-org/OpenWrt-nikki" "main"
-
-
-
 # Adguardhome
 git clone --depth=1 https://github.com/sirpdboy/luci-app-adguardhome package/luci-app-adguardhome
 sed -i "s/\(option enabled '\)1'/\10'/" package/luci-app-adguardhome/luci-app-adguardhome/root/etc/config/AdGuardHome
@@ -27,6 +19,7 @@ git clone --depth=1 https://github.com/destan19/OpenAppFilter.git package/OpenAp
 rm -rf feeds/luci/applications/luci-app-argon-config
 rm -rf feeds/luci/themes/luci-theme-argon
 git clone --depth=1 https://github.com/sbwml/luci-theme-argon package/luci-theme-argon
+git clone --depth=1 https://github.com/sbwml/luci-app-argon-config package/luci-app-argon-config
 
 
 
