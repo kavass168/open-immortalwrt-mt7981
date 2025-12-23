eb160355432f3a4bb2e4870d700e0ad8c2a24a5a
kavass168 <631638801@qq.com>
2025-12-23 10:30:44 +0800

    Update copyfile-hy3000.sh

--- scripts/copyfile-hy3000.sh
+++ scripts/copyfile-hy3000.sh
@@ -10,7 +10,14 @@ cp $GITHUB_WORKSPACE/hy3000/11_fix_wifi_mac $GITHUB_WORKSPACE/openwrt/target/lin
 cp $GITHUB_WORKSPACE/hy3000/mediatek_filogic $GITHUB_WORKSPACE/openwrt/package/boot/uboot-tools/uboot-envtools/files/
 cp $GITHUB_WORKSPACE/hy3000/hy3000.conf $GITHUB_WORKSPACE/openwrt/.config
 cp $GITHUB_WORKSPACE/hy3000/boot-mediatek-Makefile $GITHUB_WORKSPACE/openwrt/package/boot/uboot-mediatek/Makefile
+
 mkdir -p $GITHUB_WORKSPACE/openwrt/package/boot/uboot-mediatek/Makefile/src/configs
+cp $GITHUB_WORKSPACE/hy3000/mt7981_philips_hy3000_defconfig
+ $GITHUB_WORKSPACE/openwrt/package/boot/uboot-mediatek/Makefile/src/configs/
+ 
 mkdir -p $GITHUB_WORKSPACE/openwrt/package/boot/uboot-mediatek/Makefile/src/arch/arm/dts
+cp $GITHUB_WORKSPACE/hy3000/mt7981-philips-hy3000.dts
+ $GITHUB_WORKSPACE/openwrt/package/boot/uboot-mediatek/Makefile/src/arch/arm/dts
+ 
 mkdir -p $GITHUB_WORKSPACE/openwrt/package/boot/uboot-mediatek/Makefile/src/defenvs
 
