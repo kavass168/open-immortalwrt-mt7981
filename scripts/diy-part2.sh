62299374892cc4c29538ea863dff607d84104a02
kavass168 <631638801@qq.com>
2025-12-24 17:58:20 +0800

    Update diy-part2.sh

--- scripts/diy-part2.sh
+++ scripts/diy-part2.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-PKG_PATH="$GITHUB_WORKSPACE/openwrt/package/"
-
 # Adguardhome
 git clone --depth=1 https://github.com/sirpdboy/luci-app-adguardhome package/luci-app-adguardhome
 sed -i "s/\(option enabled '\)1'/\10'/" package/luci-app-adguardhome/luci-app-adguardhome/root/etc/config/AdGuardHome
@@ -17,25 +15,21 @@ git clone --depth=1 https://github.com/destan19/OpenAppFilter.git package/OpenAp
 
 
 # Argon 主题
-#rm -rf feeds/luci/applications/luci-app-argon-config
-#rm -rf feeds/luci/themes/luci-theme-argon
-#git clone --depth=1 https://github.com/sbwml/luci-theme-argon.git package/luci-theme-argon
-#git clone --depth=1 https://github.com/sbwml/luci-theme-argon/tree/openwrt-24.10/luci-app-argon-config package/luci-app-argon-config
+rm -rf feeds/luci/applications/luci-app-argon-config
+rm -rf feeds/luci/themes/luci-theme-argon
+git clone --depth=1 https://github.com/sbwml/luci-theme-argon/tree/openwrt-24.10/luci-theme-argon package/luci-theme-argon
+git clone --depth=1 https://github.com/sbwml/luci-theme-argon/tree/openwrt-24.10/luci-app-argon-config package/luci-app-argon-config
 
 # Argon 主题
-cd $PKG_PATH
+#cd $PKG_PATH
 #cd ./openwrt/package/
-git clone https://github.com/jerrykuku/luci-theme-argon.git
-git clone https://github.com/jerrykuku/luci-app-argon-config.git
+#git clone https://github.com/jerrykuku/luci-theme-argon.git
+#git clone https://github.com/jerrykuku/luci-app-argon-config.git
 
 #修改argon主题字体和颜色
-if [ -d *"luci-theme-argon"* ]; then
-	echo " "
-
-	cd ./luci-theme-argon/
-
+if [ -d openwrt/package/luci-theme-argon ]; then
+	cd ./package/luci-theme-argon
 	sed -i "s/primary '.*'/primary '#31a1a1'/; s/'0.2'/'0.5'/; s/'none'/'bing'/; s/'600'/'normal'/" ./luci-app-argon-config/root/etc/config/argon
-
-	cd $PKG_PATH && echo "theme-argon has been fixed!"
+	echo "theme-argon has been fixed!"
 fi
 
